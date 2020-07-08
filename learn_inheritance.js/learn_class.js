d39c1b421260c0322e333bbb6df69446395451a6
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-08 17:42:21 +0900

    add class myself

--- learn_inheritance.js/learn_class.js
+++ learn_inheritance.js/learn_class.js
@@ -31,3 +31,35 @@ let BGH = new PersonPlus('BGH', 10, 20, 30);
 console.log(HSH.sum());
 console.log(HSH.average())
 console.log(BGH.average());
+
+
+// class Tree {
+//     constructor(name, first, second) {
+//         this.name = name;
+//         this.first = first;
+//         this.second = second;
+//     }
+//     sum() {
+//         return this.first + this.second;
+//     }
+// }
+// let H = new Tree('H', 30, 30);
+// console.log(H.sum());
+
+// class Forest extends Tree {
+//     constructor(name, first, second, third) {
+//         super(name, first, second);
+//         this.third = third;
+//     }
+//     sum() {
+//         return super.sum() + this.third;
+//     };
+
+//     avg() {
+//         return (super.sum() + this.third) / 3;
+//     };
+// }
+
+// let GH = new Forest('GH', 30, 30, 30);
+// console.log('new : ' + GH.avg());
+
