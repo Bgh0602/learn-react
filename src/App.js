f4faefddefdb6fe5449f8a7b95299c1e81c78d47
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-10 02:10:04 +0900

    make vending machine with react

--- src/App.js
+++ src/App.js
@@ -1,82 +1,103 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-// 덧셈 뺄셈 하는 계산기 만들기
-// 기본 값은 0이다.
-// 입력하는 곳에 숫자를 입력할 수 있다.
-// 더하기 버튼을 누르면 값에 입력한 값 만큼을 더한다.
-// 빼기 버튼을 누르면 값에 입력한 값 만큼을 뺸다.
-
-// 0 [    ] [더하기] [빼기]
+const Canvas = styled.canvas`
+  border: 10px solid black;
+  width: 400px;
+  height: 500px;
+`;
+const CokeButton = styled.button`
+  position: absolute;
+  left: 70px;
+  top: 100px;
+`;
+const CiderButton = styled.button`
+  position: absolute;
+  left: 195px;
+  top: 100px;
+`;
+const FantaButton = styled.button`
+  position: absolute;
+  left: 320px;
+  top: 100px;
+`;
+const CoffeeButton = styled.button`
+  position: absolute;
+  left: 70px;
+  top: 200px;
+`;
+const CocoaButton = styled.button`
+  position: absolute;
+  left: 195px;
+  top: 200px;
+`;
+const WaterButton = styled.button`
+  position: absolute;
+  left: 320px;
+  top: 200px;
+`;
+const ChangeMoney = styled.button`
+  margin-right: -6px;
+  position: absolute;
+  left: 200px;
+  top: 290px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  background-color: white;
+  border: 1px solid blue;
+  color: blue;
+`;
+const CoinInlet = styled.button`
+  margin-left: -10px;
+  position: absolute;
+  left: 310px;
+  top: 290px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  background-color: white;
+  border: 1px solid blue;
+  color: blue;
+`;
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       list: [],
-      input: '',
-      number: '',
+      input: "",
+      number: "",
       result: 0,
     };
+    this.canvasRef = React.createRef();
+    this.ctx = null;
+  }
+
+  componentDidMount() {
+    this.ctx = this.canvasRef.current.getContext("2d");
+    this.ctx.beginPath();
+    this.ctx.moveTo(30, 30);
+    this.ctx.lineTo(270, 30);
+    this.ctx.moveTo(30, 60);
+    this.ctx.lineTo(270, 60);
+    this.ctx.moveTo(30, 90);
+    this.ctx.lineTo(270, 90);
+    this.ctx.rect(200, 95, 40, 5);
+    this.ctx.rect(30, 105, 240, 30);
+    this.ctx.stroke();
   }
 
   render() {
-    // const increase = () => {
-    //   const num = this.state.num;
-    //   this.setState({
-    //     num: num + 3,
-    //   });
-    // }
-
-    // const decrease = () => {
-    //   const num = this.state.num;
-    //   this.setState({
-    //     num: num - 3,
-    //   });
-    // }
-
     return (
       <div className="App">
-        {JSON.stringify(this.state.list)}
-        <input type="text" value={this.state.input} onChange={(event) => {
-          this.setState({
-            input: event.target.value
-          })
-        }}></input>
-        <button onClick={() => {
-          const list = this.state.list;
-          this.setState({
-            list: [...list, this.state.input],
-            input: '',
-          })
-        }}>추가</button>
-
-        <br />
-        <div>{this.state.result}</div>
-        <input type='text' value={this.state.number} onChange={(event) => {
-          this.setState({
-            number: event.target.value
-          })
-        }}></input>
-        <button onClick={() => {
-          const number = this.state.number;
-          const result = this.state.result;
-          this.setState({
-            result: Number(result) + Number(number),
-            number: '',
-          })
-        }}>
-          add
-        </button>
-        <button onClick={() => {
-          const number = this.state.number;
-          const result = this.state.result;
-          this.setState({
-            result: Number(result) - Number(number),
-            number: '',
-          })
-        }}>
-          minus
-        </button>
+        <Canvas ref={this.canvasRef} />
+        <CokeButton>coke</CokeButton>
+        <CiderButton>cider</CiderButton>
+        <FantaButton>fanta</FantaButton>
+        <CoffeeButton>coffee</CoffeeButton>
+        <CocoaButton>cocoa</CocoaButton>
+        <WaterButton>water</WaterButton>
+        <ChangeMoney>change money</ChangeMoney>
+        <CoinInlet>coin inlet</CoinInlet>
       </div>
     );
   }
