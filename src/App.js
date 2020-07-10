1078bc062d1fb45109207348f1b9d7179689523e
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-10 18:15:49 +0900

    add image of bavaerage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+import cokeImage from "./cokeImage.jpg";
+import ciderImage from "./ciderImage.jpg";
+import fantaImage from "./fantaImage.png";
+import coffeeImage from "./coffeeImage.jpg";
+import cocoaImage from "./cocoaImage.jpg";
+import waterImage from "./waterImage.jpg";
+
 const Canvas = styled.canvas`
   border: 10px solid black;
   width: 400px;
   height: 500px;
+`;
+const CurrentMoney = styled.p`
+  position: absolute;
+  left: 250px;
+  top: 230px;
 `;
 const CokeButton = styled.button`
   position: absolute;
@@ -46,6 +58,12 @@ const ChangeMoney = styled.button`
   background-color: white;
   border: 1px solid blue;
   color: blue;
+  &:hover {
+    background-color: skyblue;
+  }
+  &:active {
+    transform: translateY(3px);
+  }
 `;
 const CoinInlet = styled.button`
   margin-left: -10px;
@@ -57,16 +75,70 @@ const CoinInlet = styled.button`
   background-color: white;
   border: 1px solid blue;
   color: blue;
+  &:hover {
+    background-color: skyblue;
+  }
+  &:active {
+    transform: translateY(3px);
+  }
+`;
+const CokeImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 70px;
+  top: 30px;
+`;
+const CiderImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 195px;
+  top: 30px;
+`;
+const FantaImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 320px;
+  top: 30px;
+`;
+const CoffeeImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 70px;
+  top: 130px;
+`;
+const CocoaImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 195px;
+  top: 130px;
+`;
+const WaterImage = styled.img`
+  width: 50px;
+  height: 70px;
+  position: absolute;
+  left: 320px;
+  top: 130px;
 `;
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      list: [],
-      input: "",
-      number: "",
-      result: 0,
+      money: 0,
+      i: 0,
+      wallet: Math.floor(Math.random() * 5 + 1),
+    };
+    this.putCoin = function () {
+      if (this.state.i < this.state.wallet) {
+        this.state.money += 1000;
+        this.state.i += 1;
+      }
+      return this.state.money;
     };
     this.canvasRef = React.createRef();
     this.ctx = null;
@@ -96,8 +168,45 @@ class App extends React.Component {
         <CoffeeButton>coffee</CoffeeButton>
         <CocoaButton>cocoa</CocoaButton>
         <WaterButton>water</WaterButton>
+
+        <CoinInlet
+          onClick={() => {
+            this.putCoin();
+          }}
+        >
+          coin inlet
+        </CoinInlet>
         <ChangeMoney>change money</ChangeMoney>
-        <CoinInlet>coin inlet</CoinInlet>
+
+        <CokeImage src={cokeImage} />
+        <CiderImage src={ciderImage} />
+        <FantaImage src={fantaImage} />
+        <CoffeeImage src={coffeeImage} />
+        <CocoaImage src={cocoaImage} />
+        <WaterImage src={waterImage} />
+
+        <h3>Price List</h3>
+        <ul>
+          <li>
+            <strong>Coke</strong>:1200won
+          </li>
+          <li>
+            <strong>Cider</strong>: 1200won
+          </li>
+          <li>
+            <strong>Fanta</strong>: 900won
+          </li>
+          <li>
+            <strong>Coffee</strong>: 900won
+          </li>
+          <li>
+            <strong>Cocoa</strong>: 700won
+          </li>
+          <li>
+            <strong>Water</strong>: 700won
+          </li>
+        </ul>
+        <CurrentMoney>{this.state.money + "won"}</CurrentMoney>
       </div>
     );
   }
