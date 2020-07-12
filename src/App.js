eca86fcafaad43fca28e717b6b737933700eaff5
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-13 02:34:02 +0900

    turning colors of button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import cokeImage from "./cokeImage.jpg";
 import ciderImage from "./ciderImage.jpg";
@@ -22,6 +22,14 @@ const CokeButton = styled.button`
   position: absolute;
   left: 70px;
   top: 100px;
+  ${(props) =>
+    props.buyCoke
+      ? css`
+          background-color: #f7819f;
+        `
+      : css`
+          background-color: #ffff;
+        `}
 `;
 const CiderButton = styled.button`
   position: absolute;
@@ -125,6 +133,68 @@ const WaterImage = styled.img`
   top: 130px;
 `;
 
+class Buttons extends React.Component {
+  constructor(props) {
+    super();
+    this.giveCoke = props.giveCoke;
+    this.giveCider = props.giveCider;
+    this.giveFanta = props.giveFanta;
+    this.giveCoffee = props.giveCoffee;
+    this.giveCocoa = props.giveCocoa;
+    this.giveWater = props.giveWater;
+  }
+
+  render() {
+    return (
+      <>
+        <CokeButton
+          buyCoke={true}
+          onClick={() => {
+            this.giveCoke();
+          }}
+        >
+          coke
+        </CokeButton>
+        <CiderButton
+          onClick={() => {
+            this.giveCider();
+          }}
+        >
+          cider
+        </CiderButton>
+        <FantaButton
+          onClick={() => {
+            this.giveFanta();
+          }}
+        >
+          fanta
+        </FantaButton>
+        <CoffeeButton
+          onClick={() => {
+            this.giveCoffee();
+          }}
+        >
+          coffee
+        </CoffeeButton>
+        <CocoaButton
+          onClick={() => {
+            this.giveCocoa();
+          }}
+        >
+          cocoa
+        </CocoaButton>
+        <WaterButton
+          onClick={() => {
+            this.giveWater();
+          }}
+        >
+          water
+        </WaterButton>
+      </>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -133,13 +203,83 @@ class App extends React.Component {
       i: 0,
       wallet: Math.floor(Math.random() * 5 + 1),
     };
-    this.putCoin = function () {
-      if (this.state.i < this.state.wallet) {
-        this.state.money += 1000;
-        this.state.i += 1;
+
+    this.putCoin = () => {
+      const i = this.state.i;
+      const money = this.state.money;
+      const wallet = this.state.wallet;
+
+      if (i < wallet) {
+        this.setState({
+          money: money + 1000,
+          i: i + 1,
+        });
       }
-      return this.state.money;
     };
+
+    this.changeMoney = () => {
+      this.setState({
+        money: 0,
+      });
+    };
+
+    this.giveCoke = () => {
+      const money = this.state.money;
+
+      if (money >= 1200) {
+        this.setState({
+          money: money - 1200,
+        });
+      }
+    };
+
+    this.giveCider = () => {
+      const money = this.state.money;
+
+      if (money >= 1200) {
+        this.setState({
+          money: money - 1200,
+        });
+      }
+    };
+    this.giveFanta = () => {
+      const money = this.state.money;
+
+      if (money >= 900) {
+        this.setState({
+          money: money - 900,
+        });
+      }
+    };
+    this.giveCoffee = () => {
+      const money = this.state.money;
+
+      if (money >= 900) {
+        this.setState({
+          money: money - 900,
+        });
+      }
+    };
+    this.giveCocoa = () => {
+      const money = this.state.money;
+
+      if (money >= 700) {
+        this.setState({
+          money: money - 700,
+        });
+      }
+    };
+
+    this.giveWater = () => {
+      const money = this.state.money;
+
+      if (money > 700) {
+        this.setState({
+          money: money - 700,
+        });
+      }
+    };
+
     this.canvasRef = React.createRef();
     this.ctx = null;
   }
@@ -162,12 +302,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <Canvas ref={this.canvasRef} />
-        <CokeButton>coke</CokeButton>
-        <CiderButton>cider</CiderButton>
-        <FantaButton>fanta</FantaButton>
-        <CoffeeButton>coffee</CoffeeButton>
-        <CocoaButton>cocoa</CocoaButton>
-        <WaterButton>water</WaterButton>
+        <Buttons
+          giveWater={this.giveWater}
+          giveCocoa={this.giveCocoa}
+          giveCoffee={this.giveCoffee}
+          giveFanta={this.giveFanta}
+          giveCider={this.giveCider}
+          giveCoke={this.giveCoke}
+        />
 
         <CoinInlet
           onClick={() => {
@@ -176,7 +318,13 @@ class App extends React.Component {
         >
           coin inlet
         </CoinInlet>
-        <ChangeMoney>change money</ChangeMoney>
+        <ChangeMoney
+          onClick={() => {
+            this.changeMoney();
+          }}
+        >
+          change money
+        </ChangeMoney>
 
         <CokeImage src={cokeImage} />
         <CiderImage src={ciderImage} />
